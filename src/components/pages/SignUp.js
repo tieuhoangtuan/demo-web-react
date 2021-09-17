7f5ea84b4088ef84cb5c64acfef77b8bb23943ed
Hoang Tuan Tieu <72970689+tieuhoangtuan@users.noreply.github.com>
2021-09-17 23:53:33 +0700

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return <div className='sign-up'> </div>;
 }
